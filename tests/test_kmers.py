7e7b4c8a75099c021992ccdb1ea90d3f184303dd
Kevin Le <kevin.kent.le@gmail.com>
2019-05-09 11:41:43 -0600

    Line length

--- tests/test_kmers.py
+++ tests/test_kmers.py
@@ -10,4 +10,7 @@ from prairiedog import kmers
 
 def test_kmers_load():
     km = kmers.Kmers("tests/172.fa")
-    assert(km.headers[0] == ">gi|1062504329|gb|CP014670.1| Escherichia coli strain CFSAN004177, complete genome")
+    e = """"
+    >gi|1062504329|gb|CP014670.1| Escherichia coli strain CFSAN004177,
+     complete genome"""
+    assert(km.headers[0] == e)
