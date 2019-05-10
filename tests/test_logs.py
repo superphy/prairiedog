1581cba867b64d31846c5a68975c99fec5766b50
Kevin Le <kevin.kent.le@gmail.com>
2019-05-10 10:08:35 -0600

    Right formatting

--- tests/test_logs.py
+++ tests/test_logs.py
@@ -14,14 +14,12 @@ def test_logs():
     """Checks that logs are being recorded correctly.
     """
     msg = "Test"
-    expected = "DEBUG - {}".format(msg)
     log.debug(msg)
     found = False
     with open("prairiedog.log") as f:
-        lines = [l.rstrip() for l in f.readlines()]
-        print(lines)
-        for line in lines:
-            if expected in line:
+        for li in f:
+            line = li.rstrip()
+            if msg in line:
                 found = True
                 break
     assert found
