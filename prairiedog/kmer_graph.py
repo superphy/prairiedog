0d5213873b72ade3cb1edae0ce86c1d03612ffad
Kevin Le <kevin.kent.le@gmail.com>
2019-05-10 15:35:43 -0600

    Need to get result from future

--- prairiedog/kmer_graph.py
+++ prairiedog/kmer_graph.py
@@ -13,10 +13,15 @@ class KmerGraph:
         self._load()
 
     def _load(self):
+        """
+        Was averaging 99 to 133s for 3 genome files without the pool.
+        :return:
+        """
         with ProcessPoolExecutor() as pool:
             kmer_futures = [pool.submit(Kmers, f) for f in self.km_list]
             # Block until all Kmers loaded
-            for km in as_completed(kmer_futures):
+            for future in as_completed(kmer_futures):
+                km = future.result()
                 while km.has_next:
                     header, kmer = km.next()
                     self.graph.upsert_node(
