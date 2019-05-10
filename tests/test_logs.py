95e3522255e5cd460044ad8739cd386342fba415
Kevin Le <kevin.kent.le@gmail.com>
2019-05-10 09:45:13 -0600

    Add a check against logs

---
+++ tests/test_logs.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Tests for `prairiedog` package."""
+
+import pytest
+
+import logging
+
+log = logging.getLogger('prairiedog')
+
+
+def test_logs():
+    """Checks that logs are being recorded correctly.
+    """
+    msg = "Test"
+    expected = {"DEBUG - {}".format(msg)}
+    log.debug(msg)
+    found = False
+    with open("prairiedog.log") as f:
+        for line in [l.rstrip() for l in f.readlines()]:
+            if expected in line:
+                found = True
+                break
+    assert found
+
+
