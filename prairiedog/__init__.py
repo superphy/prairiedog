4f81fcb17223d347e4b4ff0182497b30a4f2c5d7
Kevin Le <kevin.kent.le@gmail.com>
2019-05-10 10:11:27 -0600

    Add colored logs (#11)
    * Add coloredlogs as a dep
    
    * Add a check against logs
    
    * Install core reqs as well
    
    * Add a check against logs
    
    * Log pathing
    
    * Add a check against logs
    
    * Add a check against logs
    
    * Right formatting

--- prairiedog/__init__.py
+++ prairiedog/__init__.py
@@ -5,3 +5,17 @@
 __author__ = """Kevin Le"""
 __email__ = 'kevin.kent.le@gmail.com'
 __version__ = '0.1.0'
+
+# Initialize logging
+import coloredlogs
+import logging
+
+
+log = logging.getLogger('prairiedog')
+coloredlogs.install(level='DEBUG')
+
+fmt = '%(asctime)s %(name)s[%(process)d] %(levelname)s %(message)s'
+formatter = coloredlogs.ColoredFormatter(fmt)
+fh = logging.FileHandler('prairiedog.log')
+fh.setFormatter(formatter)
+log.addHandler(fh)
