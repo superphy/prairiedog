fef926c422681829b97e698bc87072459e801764
Kevin Le <kevin.kent.le@gmail.com>
2019-05-14 14:55:13 -0600

    Include memory profiling in tox (#17)
    * Add nodes+edges for KmerGraph and a check against it
    
    * More detailed logs
    
    * Log Kmer parsing to account for multiprocessing
    
    * Check pids
    
    * Debug why were stalling on graph creation
    
    * Check graph memory usage
    
    * Check graph memory usage
    
    * Switch to external script for memory profiling
    
    * Memory profiler
    
    * Fix fixture
    
    * Fix fixture
    
    * Fix fixture
    
    * Add a pytest fixture to benchmark against increasing number of genome files
    
    * Add a pytest fixture to benchmark against increasing number of genome files
    
    * Move most genomes files to a samples/ folder so python setup.py sdist doesnt try to zip them
    
    * Directory
    
    * Change some of the log levels
    
    * Swap ordering of fixtures and see if itll pass on ci
    
    * Separate samples/ for benchmarking and tests/ for actual tests
    
    * Commit the emptyish samples/ folder
    
    * Try to disable memory profiler in ci
    
    * Try to disable memory profiler in ci
    
    * Whitelist bash
    
    * Whitelist bash

--- prairiedog/kmer_graph.py
+++ prairiedog/kmer_graph.py
@@ -1,5 +1,6 @@
 import time
 import logging
+import os
 
 from concurrent.futures import ProcessPoolExecutor, as_completed
 
@@ -10,34 +11,78 @@ log = logging.getLogger("prairiedog")
 
 
 class KmerGraph:
-    def __init__(self, km_list: list, graph: Graph):
+    def __init__(self, km_list: list, graph: Graph, k: int = None):
         # Regular class attributes
-        self.km_list = km_list  # List of genome files to parse into kmers
+        # List of genome files to parse into kmers
+        if isinstance(km_list, list):
+            self.km_list = km_list
+        elif isinstance(km_list, str):
+            self.km_list = [km_list]
         self.graph = graph
+        self.k = k
         # Load call
         self._load()
 
-    def _load(self):
-        c = 0
+    def _create_graph(self, km: Kmers) -> int:
+        log.debug("Starting to graph {} in pid {}".format(km, os.getpid()))
         st = time.time()
+        c = 0
+        while km.has_next:
+            header1, kmer1 = km.next()
+            # Create the first node
+            self.graph.upsert_node(
+                kmer1,
+                {'src': header1}
+            )
+            c += 1
+            # The same contig still has a kmer
+            while km.contig_has_next:
+                header2, kmer2 = km.next()
+                # Create the second node
+                self.graph.upsert_node(
+                    kmer2,
+                    {'src': header2}
+                )
+                # Create an edge
+                self.graph.add_edge(kmer1, kmer2)
+                # Set Kmer2 to Kmer1
+                header1, kmer1 = header2, kmer2
+                c += 1
+            # log.debug("Done processing contig {}".format(header1))
+            # At this point, we're out of kmers on that contig
+            # The loop will check if there's still kmers, and reset kmer1
+        en = time.time()
+        log.debug("Done graphing {}, covering {} kmers in {} s".format(
+            km, c, en-st))
+        return c
+
+    def _load(self):
+        st = time.time()
+        files_graphed = 0
+        log.info("Starting to create KmerGraph in pid {}".format(os.getpid()))
         with ProcessPoolExecutor() as pool:
-            kmer_futures = [pool.submit(Kmers, f) for f in self.km_list]
+            # Use the supplied K if given, otherwise default for Kmers class
+            if self.k:
+                kmer_futures = [
+                    pool.submit(Kmers, f, self.k) for f in self.km_list]
+            else:
+                kmer_futures = [
+                    pool.submit(Kmers, f) for f in self.km_list]
             # This loads each Kmer into the graph as it completes parsing, and
             # blocks until all Kmers are loaded
             for future in as_completed(kmer_futures):
+                # Get the Kmer instance from the result
                 km = future.result()
-                while km.has_next:
-                    header, kmer = km.next()
-                    self.graph.upsert_node(
-                        kmer,
-                        {'src': header}
-                    )
-                    c += 1
+                # Graph it
+                files_graphed += 1
+                log.info("{} / {}, graphing {}".format(
+                    files_graphed, len(self.km_list), km))
+                c = self._create_graph(km)
         en = time.time()
         log.info(
-            "KmerGraph took {} s to load {} files totaling {} kmers.".format(
+            "KmerGraph took {} s to load {} files totaling {} kmers".format(
                 en-st, len(self.km_list), c
             ))
-        log.info("This amounts to {} s/file or {} kmers/s.".format(
+        log.info("This amounts to {} s/file or {} kmers/s".format(
             (en-st)/len(self.km_list), c/(en-st)
             ))
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,15 @@
+import os
+import math
+import shutil
+import logging
+
 import pytest
 
 from prairiedog import kmers
 from prairiedog.networkx_graph import NetworkXGraph
 from prairiedog.kmer_graph import KmerGraph
+
+log = logging.getLogger('prairiedog')
 
 GENOME_FILES = [
     "tests/172.fa",
@@ -16,9 +23,33 @@ def genome_files():
     return GENOME_FILES
 
 
+@pytest.fixture(params=[.25, .5, .75, 1])
+def all_genome_files(request):
+    def _files(directory: str = 'samples/'):
+        fls = [directory + f for f in os.listdir(directory)
+               if f.endswith(('.fna', '.fasta', '.fa'))]
+        return fls
+
+    # Check if sample files exist
+    if _files():
+        files = _files()
+    else:
+        # Copy some files in
+        log.warning(
+            "No genomes files were found in samples/, copying from tests/")
+        test_files = _files('tests/')
+        for tf in test_files:
+            shutil.copy2(tf, 'samples/')
+        files = _files()
+    # Round up to nearest int
+    n = math.ceil(len(files) * request.param)
+    return files[: n]
+
+
 @pytest.fixture(scope="function", params=GENOME_FILES)
 def km(request):
     return kmers.Kmers(request.param)
+
 
 # TODO: use params to test against multiple backing stores
 @pytest.fixture(scope="function", params=["networkx"])
@@ -26,3 +57,13 @@ def g(request):
     if request.param == "networkx":
         return NetworkXGraph()
 
+
+@pytest.fixture
+def memory_profiler():
+    """
+    Helper function to run a pytest if the memory_profiler PyPI package is
+    installed.
+    :return:
+    """
+    memory_profiler = pytest.importorskip("memory_profiler")
+    return memory_profiler
