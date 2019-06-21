cdd52c1c4cd2018d8a92b8a54d263a9d0ece7411
Kevin Le <kevin.kent.le@gmail.com>
2019-06-21 09:55:11 -0600

    Delete dbm_map.py

--- prairiedog/edge.py
+++ prairiedog/edge.py
@@ -1,6 +1,3 @@
-from prairiedog.dbm_map import upsert
-
-
 class Edge:
     """
     Defines a set structure for creating edges
@@ -12,13 +9,6 @@ class Edge:
         self.tgt = tgt
         # Note: LemonGraph uses the type + value as str when adding, but
         # returns ordinal IDs when called
-        # try:
-        #     # Check if this is some pre-converted int from the database
-        #     int(edge_type)
-        #     self.edge_type = edge_type
-        # except ValueError:
-        #     # We were passed an actual string, so index it and convert
-        #     self.edge_type = str(upsert(edge_type, backing='dict'))
         self.edge_type = edge_type
         if edge_value == -1 and labels is not None and 'incr' in labels:
             self.edge_value = labels['incr']
@@ -36,13 +26,3 @@ class Edge:
 
     def __str__(self):
         return "prairiedog.edge.Edge with vars {}".format(vars(self))
-    #
-    # def __eq__(self, other):
-    #     if not isinstance(other, Edge):
-    #         return False
-    #     else:
-    #         # We don't just test db_id because it might not be present unless
-    #         # queried from the database
-    #         return self.edge_type == other.edge_type \
-    #                and self.edge_value == other.edge_value \
-    #                and self.incr == other.incr
