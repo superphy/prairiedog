4fa36f902057b9915f800b333228a3d54b95f308
Kevin Le <kevin.kent.le@gmail.com>
2019-06-22 09:53:35 -0600

    Check that the db file exists before setting readonly

--- prairiedog/cli.py
+++ prairiedog/cli.py
@@ -11,12 +11,13 @@ from prairiedog.lemon_graph import LGGraph, DB_PATH
 # If cli is imported, re-setup logging to level INFO
 setup_logging("INFO")
 
-# If the path doesn't exist (ie. for a tempfile for tests), LemonGraph will
+# If the path doesn't exist (ie. a tempfile for tests), LemonGraph will
 # error out if you set readonly
 if os.path.exists(DB_PATH):
     pdg = Prairiedog(g=LGGraph(readonly=True))
 else:
     pdg = Prairiedog(g=LGGraph(readonly=False))
+
 
 @click.command()
 @click.argument('src', nargs=1)
