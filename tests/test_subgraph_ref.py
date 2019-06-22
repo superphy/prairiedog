0ede78ec2721d6504443a86f70f77aa35130a2e4
Kevin Le <kevin.kent.le@gmail.com>
2019-06-22 11:37:51 -0600

    Test of full Snakemake call works, add profiler for SubgraphRef test

--- tests/test_subgraph_ref.py
+++ tests/test_subgraph_ref.py
@@ -1,16 +1,33 @@
 import pytest
 import logging
 
+from pyinstrument import Profiler
+
 from prairiedog.subgraph_ref import SubgraphRef
 from prairiedog.graph_ref import GraphRef
 
 log = logging.getLogger("prairiedog")
 
 
-def test_subgraph_creation(g, km_short):
+def _do_subgraph(g, km):
     sgr = SubgraphRef(g)
     gr = GraphRef()
 
-    sgr.update_graph(km_short, gr)
+    sgr.update_graph(km, gr)
 
     assert True
+
+
+def test_subgraph_creation(g, km_short):
+    _do_subgraph(g, km_short)
+
+
+def test_subgraph_creation_profile(g, km_short):
+    profiler = Profiler()
+    profiler.start()
+
+    _do_subgraph(g, km_short)
+
+    profiler.stop()
+
+    print(profiler.output_text(unicode=True, color=True))
