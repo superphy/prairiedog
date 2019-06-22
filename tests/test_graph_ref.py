fec05cc40cf6a6885e72c785bf20348061b32e8b
Kevin Le <kevin.kent.le@gmail.com>
2019-06-22 11:27:57 -0600

    SubgraphRef tests work, now test call of snakemake

--- tests/test_graph_ref.py
+++ tests/test_graph_ref.py
@@ -1,5 +1,3 @@
-import subprocess
-
 import pandas as pd
 
 
@@ -9,12 +7,4 @@ def test_pandas_read_mic_csv():
     assert True
 
 
-def test_graphref_output(setup_snakefile):
-    # Set the config MIC csv to use our test one
-    mic_csv = 'tests/public_mic_class_dataframe_test.csv'
 
-     # Use Snakemake to drive graph creation for two files
-    subprocess.run('snakemake --config graph_labels={}'.format(mic_csv),
-                   check=True, shell=True)
-
-    assert True
