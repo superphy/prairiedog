066f92eb54cc3fb0832367d1c06775284399bcb4
Kevin Le <kevin.kent.le@gmail.com>
2019-07-16 11:45:44 -0600

    check_output not in py3.5

--- tests/test_dgraph.py
+++ tests/test_dgraph.py
@@ -1,6 +1,12 @@
 import subprocess
+import logging
+
+log = logging.getLogger('prairiedog')
 
 
 def test_dgraph_install():
-    subprocess.run(['dgraph', '-h'], check=True, capture_output=True)
+    r = subprocess.run(
+        ['dgraph', '-h'], check=True, stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE)
+    log.debug(r)
     assert True
