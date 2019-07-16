535a4b99e357b14ad585820e43a585c9ecbc7abb
Kevin Le <kevin.kent.le@gmail.com>
2019-07-16 12:15:35 -0600

    .run returned an iterable

--- tests/test_dgraph.py
+++ tests/test_dgraph.py
@@ -7,4 +7,5 @@ log = logging.getLogger('prairiedog')
 def test_dgraph_install():
     r = subprocess.run(
         ['dgraph', '-h'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    assert 'Usage' in r
+    out = str(r)
+    assert 'Usage' in out
