82aa4c06ae55eae4319f5270392404954c1fc18f
Kevin Le <kevin.kent.le@gmail.com>
2019-07-16 11:33:50 -0600

    Try capture_output of dgraph test

--- tests/test_dgraph.py
+++ tests/test_dgraph.py
@@ -2,5 +2,5 @@ import subprocess
 
 
 def test_dgraph_install():
-    subprocess.run('dgraph -h', shell=True, check=True)
+    subprocess.run(['dgraph', '-h'], check=True, capture_output=True)
     assert True
