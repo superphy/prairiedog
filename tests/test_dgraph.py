3ba1a074982ac00066e11c9db06a33c36013c2d3
Kevin Le <kevin.kent.le@gmail.com>
2019-07-16 12:04:03 -0600

    Check for "Usage" instead of calling check=True

--- tests/test_dgraph.py
+++ tests/test_dgraph.py
@@ -6,7 +6,6 @@ log = logging.getLogger('prairiedog')
 
 def test_dgraph_install():
     r = subprocess.run(
-        ['dgraph', '-h'], check=True, stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE)
+        ['dgraph', '-h'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     log.debug(r)
-    assert True
+    assert 'Usage' in r
