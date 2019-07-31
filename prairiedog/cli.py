88d348df76bdaf7ef6aa0b71b75df71779c5876e
Kevin Le <kevin.kent.le@gmail.com>
2019-07-31 13:35:58 -0600

    Return paths meta in Dgraph, and default to Dgraph backend on cli (#125)

--- prairiedog/cli.py
+++ prairiedog/cli.py
@@ -6,21 +6,39 @@ import os
 
 from prairiedog.logger import setup_logging
 from prairiedog.prairiedog import Prairiedog
+from prairiedog.graph import Graph
 from prairiedog.lemon_graph import LGGraph, DB_PATH
+from prairiedog.dgraph import Dgraph
 
 # If cli is imported, re-setup logging to level INFO
 setup_logging("INFO")
 
-# If the path doesn't exist (ie. a tempfile for tests), LemonGraph will
-# error out if you set readonly
-if os.path.exists(DB_PATH):
-    pdg = Prairiedog(g=LGGraph(readonly=True))
-else:
-    pdg = Prairiedog(g=LGGraph(readonly=False))
+
+def connect_lemongraph() -> LGGraph:
+    # If the path doesn't exist (ie. a tempfile for tests), LemonGraph will
+    # error out if you set readonly
+    if os.path.exists(DB_PATH):
+        g = LGGraph(readonly=True)
+    else:
+        g = LGGraph(readonly=False)
+    return g
+
+
+def parse_backend(backend: str) -> Graph:
+    if backend == 'dgraph':
+        g = Dgraph()
+    elif backend == 'lemongraph':
+        g = connect_lemongraph()
+    else:
+        g = Dgraph()
+    return g
 
 
 @click.command()
 @click.argument('src', nargs=1)
 @click.argument('dst', nargs=1)
-def query(src: str, dst: str):
+@click.option('--backend', default='dgraph', help='Backend graph database')
+def query(src: str, dst: str, backend: str):
+    g = parse_backend(backend)
+    pdg = Prairiedog(g=g)
     pdg.query(src, dst)
