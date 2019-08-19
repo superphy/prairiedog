ac03d4c66e508e77467cd0a5dbcc0a04e4e35da1
Kevin Le <kevin.kent.le@gmail.com>
2019-08-19 15:39:50 -0600

    Add log messages if nodes exist

--- tests/test_dgraph_bulk.py
+++ tests/test_dgraph_bulk.py
@@ -1,9 +1,13 @@
 import tempfile
 import pathlib
+import logging
 
 from prairiedog.node import Node
 from prairiedog.dgraph_bundled import DgraphBundled
 from prairiedog.errors import GraphException
+
+
+log = logging.getLogger('prairiedog')
 
 
 def test_dgraph_bulk_basics(dgraph_bundled_helper):
@@ -11,11 +15,13 @@ def test_dgraph_bulk_basics(dgraph_bundled_helper):
     tmp_dir = tempfile.mkdtemp()
     r1 = pathlib.Path(tmp_dir, 'r1.rdf')
     with open(r1, 'a') as f:
-        f.write('_:a <km> "ATCG" . ')
-        f.write('_:b <km> "ATGC" . ')
+        f.write('_:a <km> "ATCG" . \n')
+        f.write('_:b <km> "ATGC" . \n')
     dg.load(tmp_dir)
     assert dg.g.exists_node(Node(value="ATCG"))
+    log.info("ATCG exists")
     assert dg.g.exists_node(Node(value="ATGC"))
+    log.info("ATGC exists")
 
 # TODO: get this to run on CircleCI without being killed
 # def test_dgraph_bulk_snakemake(dgraph_build: DgraphBundled):
